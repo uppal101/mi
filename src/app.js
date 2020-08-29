a3e788206e71062000acad68707204d2e638915d
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2020-08-29 16:56:42 -0500

    As ready as can be with the time block

--- src/app.js
+++ src/app.js
@@ -1,3 +1,113 @@
-import { convertDate } from "./utils";
+import { getDate, getConsecutiveDays } from "./utils";
+import cloudy from '../img/cloudy.png';
+import sunny from '../img/sunny.png';
+import snow from '../img/snow.png';
+import rain from '../img/rain.png';
 
-console.log("hello world");
+let city;
+let weatherJson;
+let zipcode = document.getElementById('zipcode');
+let submit = document.querySelector('button');
+
+submit.addEventListener('click', function(evt){
+  if (zipcode.value.length < 5) {
+    alert ("Please enter a 5 digit zipcode");
+  } else {
+    fetch(`https://se-weather-api.herokuapp.com/api/v1/geo?zip_code=${zipcode.value}`)
+      .then(function(geoPromise){
+        return geoPromise.json();
+      })
+      .then(function(geoObj){
+          city = geoObj.city;
+          fetch(`https://se-weather-api.herokuapp.com/api/v1/forecast?latitude=${geoObj.latitude}&longitude=${geoObj.longitude}&date=${getDate()}`)
+            .then(function(weatherPromise){
+              return weatherPromise.json();
+            }).then(function(weatherObj) {
+              weatherJson = weatherObj;
+              renderWeather();
+            });
+      });
+  };
+});
+
+function renderWeather() {
+  renderHeader();
+  renderWeatherForecast();
+};
+
+function renderHeader() {
+  const header = document.createElement('h3');
+  header.textContent = `WEATHER FORECAST FOR ${city.toUpperCase()}`;
+  header.setAttribute('id', 'header');
+
+  const mainPage = document.getElementById('app');
+  mainPage.appendChild(header);
+};
+
+function renderWeatherForecast() {
+  const threeDays = weatherJson.daily.data.slice(0,3);
+  const weatherHeader = document.getElementById('header');
+  const forecast = document.createElement('div');
+  forecast.setAttribute('class', 'flex');
+
+  threeDays.map((day, index) => {
+    let container = document.createElement('div');
+    container.setAttribute('class', 'container');
+    let headerDay = document.createElement('div');
+    headerDay.setAttribute('class', 'dayHeader');
+    headerDay.textContent = getDayText(index);
+
+    let weatherContainer = document.createElement('div');
+    weatherContainer.setAttribute('class', 'flex');
+    let img = document.createElement('img');
+    img.src = getImageIcon(day.icon);
+
+    let weatherContentContainer = document.createElement('div');
+    let weatherSummary = document.createElement('div');
+
+    let highTemp = document.createElement('span');
+    highTemp.setAttribute('class', 'bold');
+    let lowTemp = document.createElement('span');
+    weatherSummary.textContent = day.summary;
+    highTemp.textContent = `${Math.round(day.temperatureHigh)}°`;
+    lowTemp.textContent = `/ ${Math.round(day.temperatureLow)}° F`;
+
+    weatherContentContainer.appendChild(weatherSummary);
+    weatherContentContainer.appendChild(highTemp);
+    weatherContentContainer.appendChild(lowTemp);
+
+    weatherContainer.appendChild(img);
+    weatherContainer.appendChild(weatherContentContainer);
+
+    container.appendChild(headerDay);
+    container.appendChild(weatherContainer);
+
+    forecast.appendChild(container);
+    weatherHeader.appendChild(forecast);
+  });
+};
+
+function getDayText(number) {
+  const consecDays = getConsecutiveDays();
+
+  if (number < 1) {
+    return 'Today:';
+  };
+  return consecDays[number - 1];
+};
+
+function getImageIcon(icon) {
+  switch(icon) {
+    case 'cloudy':
+      return cloudy;
+      break;
+    case 'rain':
+      return rain;
+      break;
+    case 'snow':
+      return snow;
+      break;
+    default:
+      return sunny;
+  };
+};
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,30 @@
-/**
- *
- * @param {number} time - Unix time in seconds returns it in miliseconds
- */
-export function convertDate(time) {
-  return time * 1000;
+export function getDate() {
+ let date = new Date().toJSON().slice(0,10);
+ let monthDate = date.slice(5);
+ let year = date.slice(0,4);
+
+ return `${monthDate}-${year}`;
+};
+
+export function getConsecutiveDays() {
+  const date = new Date();
+  const weekday = new Array(7);
+  weekday[0] = "Sunday:";
+  weekday[1] = "Monday:";
+  weekday[2] = "Tuesday:";
+  weekday[3] = "Wednesday:";
+  weekday[4] = "Thursday:";
+  weekday[5] = "Friday:";
+  weekday[6] = "Saturday:";
+
+  switch(date.getDay()) {
+    case 6:
+      return [weekday[0], weekday[1]];
+      break;
+    case 5:
+      return [weekday[date.getDay() + 1], weekday[0]];
+      break;
+    default:
+      return [weekday[date.getDay() + 1], weekday[date.getDay() + 2]];
+  }
 }
